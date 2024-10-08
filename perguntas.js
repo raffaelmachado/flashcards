062efe5448317d127fb731159d485f3cb446f8b1
Raffa <133163274+raffaelmachado@users.noreply.github.com>
2024-10-08 12:04:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,284 @@
+criaCartao(
+    'Arte',
+    'Qual a obra mais famosa de Van Gogh? ',
+    'noite estrelada'
+)
+
+criaCartao(
+    'Ciências',
+    'Quantos minutos leva para a luz do Sol chegar na Terra?',
+    '8 minutos'
+)
+
+criaCartao(
+    'Games',
+    ' De qual personagem de mortal Kombat é a frase "o combate não é para palhaços"?',
+    'Kitana'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão',
+    'Tokio'
+)
+
+criaCartao(
+    'Química',
+    'qual é a sigla do cobre na tabela periódica?',
+    'Cu'
+)
+
+criaCartao(
+    'ingês',
+    'o que significa beautiful, sea em português?',
+    ' lindo, mar'
+)
+
+criaCartao(
+    'História',
+    'onde os jogos olímpicos foram criados?',
+    'Olímpia, Grécia'
+)
+criaCartao(
+    'mitologia',
+    'Quem é o Deus do caus na mitologia egipicia ',
+    'Apophis'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos ossos tem no corpo humano?',
+    '206'
+)
+criaCartao(
+    'Geografia',
+    'Quantos continentes existem?',
+    '6'
+)
+criaCartao(
+    'Automotivo',
+    'Qual o animal pesente no logo da ferrari?',
+    'cavalo'
+)
+criaCartao(
+    'História',
+    'qual o nome do inventor do telefone? ',
+    'Alexandre Graham Bell'
+)
+criaCartao(
+    'Química',
+    'Qual é o nome popular do cloreto de sódio? ',
+    'Sal de cozinha'
+)
+
+criaCartao(
+    'História',
+    'Em que pais foi cronstruido o muro de Berlim? ',
+    'Alemanha'
+)
+criaCartao(
+    'Matemática',
+    'Qual a metade de 2x8? ',
+    '8'
+)
+criaCartao(
+    'Série',
+    'Qual é o nome da meninas suer poderosas?',
+    'Docinho, Lindinha e Florzinha'
+)
+criaCartao(
+    'Filme',
+    'Qual o nome do carro mais veloz de todos os desnhos? ',
+    'Relâmpago MCqueen'
+)
+criaCartao(
+    'Filme',
+    'Qual o lema do Timão e Pumba? ',
+    'Hakuna Matata'
+)
+criaCartao(
+    'Geografia',
+    'Em qualcontinente se localiza o Mexico? ',
+    'America'
+)
+criaCartao(
+    'Artes',
+    'Quem pintou o quadro Monalisa? ',
+    'Leonardo da Vinci'
+)
+criaCartao(
+    'História',
+    'Qual cidade é conhecida como cidade eterna? ',
+    'Roma'
+)
+
+criaCartao(
+    'Filme',
+    'Qual o nome do principal vilão de Harry Potter? ',
+    'Voldermort'
+)
+criaCartao(
+    'Curiosidade',
+    'Qual a flor nacional do Japão? ',
+    'Sakura/Flor de cerejeira'
+)
+criaCartao(
+    'Filme',
+    'Qualo nome do martelodo Thor? ',
+    'Mjolnir'
+)
+criaCartao(
+    'musica',
+    'Qual o artista conhecido como rei do pop? ',
+    'Michael Jackson'
+)
+criaCartao(
+    'curiosidade',
+    'Qual o nome do programa de culinária mais conhcido do Brasil? ',
+    'Masterchef'
+)
+criaCartao(
+    'Jogos',
+    'Qual vilão da nintendo aparece repetidamente pra sequestrar a princesa peach? ',
+    'Bowser'
+)
+criaCartao(
+    'matemática',
+    'Quanto é 3+8x8? ',
+    '67'
+)
+criaCartao(
+    'inglês',
+    'Como se  fala estrela em inglês? ',
+    'Star'
+)
+criaCartao(
+    'Geografia ',
+    'Qual a maior montanha do mundo? ',
+    'Monte Everest'
+)
+criaCartao(
+    'Geografia',
+    'Qual pais tem formato de bota? ',
+    'Itália'
+)
+criaCartao(
+    'Curiosidade',
+    'Qual o personagem mais famoso de Mauricio de Souza? ',
+    'Mônica'
+)
+criaCartao(
+    'Curi0osidades',
+    'Qual fruta nasce da oliveira? ',
+    'Azeitona'
+)
+criaCartao(
+    'história',
+    'Em que pais se localiza o Taj Mahal? ',
+    'Indía'
+)
+criaCartao(
+    'inglês',
+    'Como se fala pão em inglês? ',
+    'bread'
+)
+criaCartao(
+    'Geografia',
+    'Qual o maior rio do Brasil? ',
+    'Amazonas'
+)
+criaCartao(
+    'Cidadania',
+    'Que dia é comemorado o dia da bandeira no Brasil? ',
+    '19 de novembro'
+)
+criaCartao(
+    'Cidadania',
+    'Que dia é comemorado o dia da bandeira no Brasil? ',
+    '19 de novembro'
+)
+criaCartao(
+    'Cidadania',
+    'Que dia é comemorado o dia da bandeira no Brasil? ',
+    '19 de novembro'
+)
+criaCartao(
+    'Cidadania',
+    'Que dia é comemorado o dia da bandeira no Brasil? ',
+    '19 de novembro'
+)
+criaCartao(
+    'Cidadania',
+    'Que dia é comemorado o dia da bandeira no Brasil? ',
+    '19 de novembro'
+)
+criaCartao(
+    'Cidadania',
+    'Que dia é comemorado o dia da bandeira no Brasil? ',
+    '19 de novembro'
+)
+criaCartao(
+    'Cidadania',
+    'Que dia é comemorado o dia da bandeira no Brasil? ',
+    '19 de novembro'
+)
+criaCartao(
+    'Cidadania',
+    'Que dia é comemorado o dia da bandeira no Brasil? ',
+    '19 de novembro'
+)
+criaCartao(
+    'Cidadania',
+    'Que dia é comemorado o dia da bandeira no Brasil? ',
+    '19 de novembro'
+)
+criaCartao(
+    'Cidadania',
+    'Que dia é comemorado o dia da bandeira no Brasil? ',
+    '19 de novembro'
+)
+criaCartao(
+    'Cidadania',
+    'Que dia é comemorado o dia da bandeira no Brasil? ',
+    '19 de novembro'
+    )
+    criaCartao(
+        'Cidadania',
+        'Que dia é comemorado o dia da bandeira no Brasil? ',
+        '19 de novembro'
+    )
+    criaCartao(
+        'Cidadania',
+        'Que dia é comemorado o dia da bandeira no Brasil? ',
+        '19 de novembro'
+    )
+    criaCartao(
+        'Cidadania',
+        'Que dia é comemorado o dia da bandeira no Brasil? ',
+        '19 de novembro'
+    )
+    criaCartao(
+        'Cidadania',
+        'Que dia é comemorado o dia da bandeira no Brasil? ',
+        '19 de novembro'
+    )
+    criaCartao(
+        'Cidadania',
+        'Que dia é comemorado o dia da bandeira no Brasil? ',
+        '19 de novembro'
+    )
+    criaCartao(
+        'Cidadania',
+        'Que dia é comemorado o dia da bandeira no Brasil? ',
+        '19 de novembro'
+    )
+    criaCartao(
+        'Cidadania',
+        'Que dia é comemorado o dia da bandeira no Brasil? ',
+        '19 de novembro'
+    )
+    criaCartao(
+        'Cidadania',
+        'Que dia é comemorado o dia da bandeira no Brasil? ',
+        '19 de novembro'
+    )
